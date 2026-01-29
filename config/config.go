db1e92f21117830e157065d47df2d79778b49a17
Alkush-Pipania <133737222+Alkush-Pipania@users.noreply.github.com>
2026-01-30 03:52:20 +0530

    fix

--- config/config.go
+++ config/config.go
@@ -32,7 +32,7 @@ type Config struct {
 func LoadEnv() *Config {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	return &Config{
